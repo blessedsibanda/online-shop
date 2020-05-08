4027b302bc5af911978fea510a1b6ceceea91cc4
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2020-05-08 03:32:56 +0200

    show product counts  in sidebar

--- django/shop/views.py
+++ django/shop/views.py
@@ -6,6 +6,7 @@ def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    total_products = products.count()
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
@@ -14,7 +15,8 @@ def product_list(request, category_slug=None):
                   {
                       'category': category,
                       'categories': categories,
-                      'products': products
+                      'products': products,
+                      'total_products': total_products
                   })
 
 
